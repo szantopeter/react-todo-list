fcfa2858ec559b1ba1e27332d0abec1e485ea24f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-15 10:33:33 +0000

    Deploy szantopeter/react-todo-list to github.com/szantopeter/react-todo-list.git:gh-pages

---
+++ precache-manifest.3fc303448104226568d2aba10533eed9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b9ba2a1ad2866c692876e8d5f11975",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46b8cca4cc86342937df",
+    "url": "./static/css/main.6ea9fc13.chunk.css"
+  },
+  {
+    "revision": "f8a3aa178cfae837a1f0",
+    "url": "./static/js/2.63e0bee9.chunk.js"
+  },
+  {
+    "revision": "46b8cca4cc86342937df",
+    "url": "./static/js/main.1ac120a7.chunk.js"
+  },
+  {
+    "revision": "a015a154d74c653fe4d8",
+    "url": "./static/js/runtime-main.2794132c.js"
+  }
+]);
